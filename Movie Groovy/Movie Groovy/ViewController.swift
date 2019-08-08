3460865958ae179643c68d9e64a9dbb52919fd78
Nelli <nelli.galiullina@redone.app>
2019-08-08 14:58:36 +0500

    Create project

---
+++ Movie Groovy/Movie Groovy/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Movie Groovy
+//
+//  Created by admin on 08/08/2019.
+//  Copyright © 2019 admin. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
