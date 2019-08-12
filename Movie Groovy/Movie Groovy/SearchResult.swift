934400726db8e73f347bbdb926cb08a536f2d39d
Roman Syromyatnikov <roman.syromyatnikov@redone.app>
2019-08-12 13:27:08 +0500

    file change

--- Movie Groovy/Movie Groovy/SearchResult.swift
+++ Movie Groovy/Movie Groovy/SearchResult.swift
@@ -7,3 +7,29 @@
 //
 
 import Foundation
+class ResultArray:Codable {
+    var page = 0
+    var results = [SearchResult]()
+    var totalPages = 0
+    var totalResults = 0
+}
+class SearchResult:Codable {
+    var poster_path: String? = ""
+    var adult = false
+    var overview: String = ""
+    var release_date: String = ""
+    var genre_ids: [Int] = []
+    var id = 0
+    var original_Title: String = ""
+    var original_language: String = ""
+    var title: String = ""
+    var backdrop_path: String? = ""
+    var popularity = 0.0
+    var vote_count = 0
+    var video = false
+    var vote_average = 0.0
+    
+    /* var name:String {
+        return trackName ?? ""
+    }   */
+}
