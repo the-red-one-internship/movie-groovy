c038d63aa5a53e571fdc356b39bd5c96f337961a
Nelli <nelli.galiullina@redone.app>
2019-08-12 10:36:00 +0500

    create files

---
+++ Movie Groovy/Movie Groovy/PosterCell.swift
@@ -0,0 +1,9 @@
+//
+//  File.swift
+//  Movie Groovy
+//
+//  Created by admin on 09/08/2019.
+//  Copyright © 2019 admin. All rights reserved.
+//
+
+import Foundation
---
+++ Movie Groovy/Movie Groovy/SearchCollectionReusableView.swift
@@ -0,0 +1,9 @@
+//
+//  SearchCollectionReusableView.swift
+//  Movie Groovy
+//
+//  Created by admin on 09/08/2019.
+//  Copyright © 2019 admin. All rights reserved.
+//
+
+import Foundation
