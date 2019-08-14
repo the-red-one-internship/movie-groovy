2c16ac34fc673868ef54e468ee0c97134b177bde
Nelli <nelli.galiullina@redone.app>
2019-08-14 15:32:49 +0500

    create LanguageViewController

---
+++ Movie Groovy/Movie Groovy/TransitionAnimator.swift
@@ -0,0 +1,13 @@
+//
+//  TransitionAnimator.swift
+//  Movie Groovy
+//
+//  Created by admin on 14/08/2019.
+//  Copyright © 2019 admin. All rights reserved.
+//
+
+import UIKit
+
+class TransitionAnimator: NSObject {
+
+}
