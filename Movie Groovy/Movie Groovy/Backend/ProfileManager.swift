083e277f4d887187e583465dff957bc74e12d3bb
Nelli <nelli.galiullina@redone.app>
2019-08-20 13:18:17 +0500

    refactor profile manager

---
+++ Movie Groovy/Movie Groovy/Backend/ProfileManager.swift
@@ -0,0 +1,9 @@
+//
+//  ProfileManager.swift
+//  Movie Groovy
+//
+//  Created by admin on 19/08/2019.
+//  Copyright © 2019 admin. All rights reserved.
+//
+
+import Foundation
---
+++ Movie Groovy/Movie Groovy/ExploreViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  ExploreViewCell.swift
+//  Movie Groovy
+//
+//  Created by admin on 20/08/2019.
+//  Copyright © 2019 admin. All rights reserved.
+//
+
+import UIKit
+
+class ExploreViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
