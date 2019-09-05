1d94786365d9d112d9cc14769b5f34d04aa6de4b
Nelli <nelli.galiullina@redone.app>
2019-09-05 17:30:29 +0500

    fix constrain

--- Movie Groovy/Movie Groovy/ChangePasswordViewController.swift
+++ Movie Groovy/Movie Groovy/ChangePasswordViewController.swift
@@ -16,9 +16,48 @@ class ChangePasswordViewController: UIViewController, UIViewControllerTransition
     @IBOutlet weak var currentPassword: UITextField!
     @IBOutlet weak var newPassword: UITextField!
     @IBOutlet weak var confirmNewPassword: UITextField!
+    @IBOutlet weak var currentPasswordBottomConstraint: NSLayoutConstraint!
+    @IBOutlet weak var newPasswordBottomConstraint: NSLayoutConstraint!
+    @IBOutlet weak var confirmNewPasswordBottomConstraint: NSLayoutConstraint!
+    
     
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillShow), name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.addObserver(self, selector: #selector(self.keyboardWillHide), name: UIResponder.keyboardWillHideNotification, object: nil)
+    }
+    
+    @objc func keyboardWillShow(notification: Notification) {
+        adjustingHeight(show: true, notification: notification)
+    }
+    
+    @objc func keyboardWillHide(notification: Notification) {
+        adjustingHeight(show: false, notification: notification)
+    }
+    
+    func adjustingHeight(show: Bool, notification: Notification) {
+        var  userInfo = notification.userInfo!
+        let keyboardframe: CGRect = (userInfo[UIResponder.keyboardFrameBeginUserInfoKey] as! NSValue).cgRectValue
+        let animationDuration = userInfo[UIResponder.keyboardAnimationDurationUserInfoKey] as! TimeInterval
+        let changeInHeight = (keyboardframe.height + 1) * (show ? 1 : -1)
+        print(changeInHeight)
+        print(self.confirmNewPasswordBottomConstraint.constant)
+        UIView.animate(withDuration: animationDuration, animations: { () -> Void in
+            self.confirmNewPasswordBottomConstraint.constant += changeInHeight
+        })
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillShowNotification, object: nil)
+        NotificationCenter.default.removeObserver(self, name: UIResponder.keyboardWillHideNotification, object: nil)
+
+    }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
     }
     
     @IBAction func saveTapped(_ sender: Any) {
